9ad843cb207cef05496104cc61a8bad510791e28
Thongchai Subsaidee <chanokture@me.com>
2025-07-30 05:37:24 +0700

    Initial Commit

---
+++ NotificationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationDemo
+//
+//  Created by Thongchai Subsaidee on 30/7/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationDemo/NotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationDemoApp.swift
+//  NotificationDemo
+//
+//  Created by Thongchai Subsaidee on 30/7/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct NotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
